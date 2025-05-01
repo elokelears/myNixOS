b68aeeba103c674bf94defd143df6f953ffc25b3
elokelears <elokelears@gmail.com>
2025-05-01 15:22:22 +0000

    update nvidia

--- host/machines/desktop-nvidia/nvidia.nix
+++ host/machines/desktop-nvidia/nvidia.nix
@@ -4,14 +4,12 @@
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
 
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     modesetting.enable = true; # 推荐开启
     powerManagement.enable = false; # 按需开启
-    # open = false; # 默认不需要设置
+    open = false; # 默认不需要设置
     # prime = { ... }; # 如果是混合显卡
   };
 
