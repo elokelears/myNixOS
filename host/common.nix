25786b3b3c80c7e0da0a692838326a64afb88521
elokelears <elokelears@gmail.com>
2025-05-02 15:39:47 +0800

    update fontConfig

--- host/common.nix
+++ host/common.nix
@@ -42,6 +42,26 @@
     ];
   };
 
+  # FontConfig
+  fonts = {
+    packages = with pkgs; [
+      fira-code-nerdfont
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
+     ];
+    fontconfig = {
+      antialias = true;
+      hinting.enable = true;
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [ "FiraCode Nerd Font" ];
+        sansSerif = [ "Noto Sans CJK SC" ];
+        serif = [ "Noto Serif CJK SC" ];
+       };
+     };
+   };
+
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
