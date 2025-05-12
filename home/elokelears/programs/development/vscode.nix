5b2307dd5d6291205fae4adf49df3e71b81abc6a
elokelears <elokelears@gmail.com>
2025-05-13 03:05:32 +0800

    update nix-ide

--- home/elokelears/programs/development/vscode.nix
+++ home/elokelears/programs/development/vscode.nix
@@ -3,5 +3,12 @@
 {
   programs.vscode = {
     enable = true;
+    profiles = {
+      "default" = {
+        extensions = [
+          pkgs.vscode-extensions.jnoortheen.nix-ide
+        ];
+      }
+    };
   };
 }
