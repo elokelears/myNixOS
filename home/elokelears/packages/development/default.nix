4d635fccc99343639342a2b4496ba3111f1d782f
elokelears <elokelears@gmail.com>
2025-05-13 16:09:04 +0800

    enable dingtalk

--- home/elokelears/packages/development/default.nix
+++ home/elokelears/packages/development/default.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, nur, ... }:
 
 {
     home.packages = [
         pkgs.windsurf
         pkgs.code-cursor
         pkgs.jetbrains.idea-ultimate
+        nur.repos.xddxdd.dingtalk
     ];
 }
